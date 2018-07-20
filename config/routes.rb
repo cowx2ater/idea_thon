848ac4b64d3a57af2533a7a1974016454202b505
yeongjo <playinlion@likelion.org>
2018-07-21 03:22:45 +0900

    랭킹보기 뷰

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  root 'home#index'
+
+  resources :home, only: [:index, :show]
+
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
