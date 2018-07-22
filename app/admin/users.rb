ec7305a6f94d4d4409288a455f98c3913030a045
wonno <jongwonno@likelion.org>
2018-07-22 18:24:23 +0900

    fsfs

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -1,6 +1,6 @@
 ActiveAdmin.register User do
   actions :all, except: [:new,:create,:destroy]
-  
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
@@ -13,23 +13,23 @@ ActiveAdmin.register User do
 #   permitted << :other if params[:action] == 'create' && current_user.admin?
 #   permitted
 # end
-scope :all
-scope :no_team
-scope :tutor
-scope :student
+# scope :all
+# scope :no_team
+# scope :tutor
+# scope :student
 
 filter :team
 filter :name
 filter :univ, as: :select, collection: User.all.pluck("univ").uniq.sort
-filter :year, as: :select, collection: User.all.pluck("year").uniq.sort
-filter :role, as: :select, collection: User.all.pluck("role").uniq
+# filter :year, as: :select, collection: User.all.pluck("year").uniq.sort
+# filter :role, as: :select, collection: User.all.pluck("role").uniq
 
 index do
   selectable_column
   # id_column
   # column :lion_id
-  column :year
-  column :role
+  # column :year
+  # column :role
   column :univ
   column :name
   column :email
