68d4a68c00d695d112d25b7470b81e39fcd31c7c
wonno <jongwonno@likelion.org>
2018-07-22 18:37:39 +0900

    reload

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -13,23 +13,23 @@ ActiveAdmin.register User do
 #   permitted << :other if params[:action] == 'create' && current_user.admin?
 #   permitted
 # end
-# scope :all
-# scope :no_team
-# scope :tutor
-# scope :student
+scope :all
+scope :no_team
+scope :tutor
+scope :student
 
 filter :team
 filter :name
 filter :univ, as: :select, collection: User.all.pluck("univ").uniq.sort
-# filter :year, as: :select, collection: User.all.pluck("year").uniq.sort
-# filter :role, as: :select, collection: User.all.pluck("role").uniq
+filter :year, as: :select, collection: User.all.pluck("year").uniq.sort
+filter :role, as: :select, collection: User.all.pluck("role").uniq
 
 index do
   selectable_column
   # id_column
   # column :lion_id
-  # column :year
-  # column :role
+  column :year
+  column :role
   column :univ
   column :name
   column :email
